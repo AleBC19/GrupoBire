78022f10b96e22ba8d19983de10e817746c36525
Robert <roberttv17@gmail.com>
2022-12-18 22:35:26 -0600

    paint services and styles

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include_once './templates/head.php'; ?>
 
-<section>
+<section class="container">
     <h2 class="text-center">¿Quienes Somos?</h2>
 </section>
 
-<section>
+<section class="container">
     <h2 class="text-center">Nuestros Servicios</h2>
+    <div id="services"></div>
 </section>
 
 <?php include_once './templates/footer.php'; ?>
--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 import servicios from './servicios.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(servicios)
+    initApp();
 });
+
+function initApp() {
+    paintService();
+}
+
+const paintService = () => {
+    getServices( servicios );
+}
+
+const getServices = ( services ) => { 
+    services.forEach( service => {
+        const { id, titulo, contenido } = service;
+
+        const title = document.createElement('H3');
+        title.textContent = titulo;
+
+        const content = document.createElement('P');
+        content.textContent = contenido;
+
+        const serviceDiv = document.createElement('DIV');
+        serviceDiv.classList.add('service');
+        serviceDiv.appendChild(title);
+        serviceDiv.appendChild(content);
+
+        document.querySelector('#services').appendChild(serviceDiv);
+    });
+}
